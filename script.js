716702cc8636af2d344807909faf42ef6b59ea69
Emoreira32 <dinhomoreira32@hotmail.com>
2021-05-21 14:13:32 -0400

    js fixes

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ timer = null;
 function start() {
     if (!timer) {
         timer = setInterval(setTime, 1000);
+    } else {
+        // timer reaches 0
+        document.querySelector('section.start').style.display = 'none';
+        document.querySelector('section.q1').style.display = 'none';
+        document.querySelector('section.q2').style.display = 'none';
+        document.querySelector('section.q3').style.display = 'none';
+        document.querySelector('section.q4').style.display = 'none';
+        document.querySelector('section.end').style.display = 'block';
     }
 }
 
@@ -47,74 +55,91 @@ function pad(val) {
 
 // custom
 
+var score = 0;
+
+// output result
+function save() {
+    document.querySelector('.title').style.display = 'none';
+    document.querySelector('.name').style.display = 'none';
+
+    document.querySelector('.result').style.display = 'block';
+    document.querySelector('.result > h1').textContent = "Hi " + document.querySelector('input[name="name"]').value + " you scored " + Math.floor((score/4)*100) + "%";
+}
+
+// check answer
 function check(q) {
+    console.log(q);
     switch(q) {
-    	case 'q1':
-    		if (document.querySelector('input[name="q1"]:checked').value == 'B') {
+        case 'q1':
+            if (document.querySelector('input[name="q1"]:checked').value == 'B') {
                 document.querySelector('input[name="q1"]:checked+label').style.color = "green";
                 setTimeout(function() {
                     document.querySelector('.q1').style.display = 'none';
                     document.querySelector('.q2').style.display = 'block';
                 }, 1600);
-    		} else {
-        		document.querySelector('input[name="q1"]:checked+label').style.color = "red";
+                score++;
+            } else {
+                document.querySelector('input[name="q1"]:checked+label').style.color = "red";
                 setTimeout(function() {
                     document.querySelector('.q1').style.display = 'none';
                     document.querySelector('.q2').style.display = 'block';
                 }, 1600);
                 totalSeconds -= 30;
-    		}
-    		break;
-    	case 'q2':
-    		if (document.querySelector('input[name="q2"]:checked').value == 'A') {
+            }
+            break;
+        case 'q2':
+            if (document.querySelector('input[name="q2"]:checked').value == 'A') {
                 document.querySelector('input[name="q2"]:checked+label').style.color = "green";
                 setTimeout(function() {
                     document.querySelector('.q2').style.display = 'none';
                     document.querySelector('.q3').style.display = 'block';
                 }, 1600);
-    		} else {
-        		document.querySelector('input[name="q2"]:checked+label').style.color = "red";
+                score++;
+            } else {
+                document.querySelector('input[name="q2"]:checked+label').style.color = "red";
                 setTimeout(function() {
                     document.querySelector('.q2').style.display = 'none';
                     document.querySelector('.q3').style.display = 'block';
                 }, 1600);
                 totalSeconds -= 30;
-    		}
-    		break;
+            }
+            break;
         case 'q3':
-    		if (document.querySelector('input[name="q3"]:checked').value == 'C') {
+            if (document.querySelector('input[name="q3"]:checked').value == 'C') {
                 document.querySelector('input[name="q3"]:checked+label').style.color = "green";
                 setTimeout(function() {
                     document.querySelector('.q3').style.display = 'none';
                     document.querySelector('.q4').style.display = 'block';
                 }, 1600);
-    		} else {
-        		document.querySelector('input[name="q3"]:checked+label').style.color = "red";
+                score++;
+            } else {
+                document.querySelector('input[name="q3"]:checked+label').style.color = "red";
                 setTimeout(function() {
                     document.querySelector('.q3').style.display = 'none';
                     document.querySelector('.q4').style.display = 'block';
                 }, 1600);
                 totalSeconds -= 30;
-    		}
-    		break;
+            }
+            break;
         case 'q4':
-    		if (document.querySelector('input[name="q4"]:checked').value == 'B') {
+            if (document.querySelector('input[name="q4"]:checked').value == 'B') {
                 document.querySelector('input[name="q4"]:checked+label').style.color = "green";
                 setTimeout(function() {
                     document.querySelector('.q4').style.display = 'none';
                     document.querySelector('.end').style.display = 'block';
                 }, 1600);
-    		} else {
-        		document.querySelector('input[name="q4"]:checked+label').style.color = "red";
+                score++;
+            } else {
+                document.querySelector('input[name="q4"]:checked+label').style.color = "red";
                 setTimeout(function() {
                     document.querySelector('.q4').style.display = 'none';
                     document.querySelector('.end').style.display = 'block';
                 }, 1600);
                 stop();
-    		}
-    		break;
-    	default:
-    		//
-    		break;
+            }
+            break;
+        default:
+            //
+            break;
     }
 }
